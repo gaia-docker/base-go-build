5d50a954698d14d3983c3aa7f72e0b7e7ad9b28a
Effi <effoeffi@gmail.com>
2016-12-31 17:04:56 +0200

    logs;

--- script/go_build.sh
+++ script/go_build.sh
@@ -2,10 +2,18 @@
 distdir=.dist
 
 go_build() {
+    echo " ---- before rm ---- "
+    ls -la
   rm -rf "${distdir}"
+  echo " ---- before mkdir ---- "
+  ls -la
   mkdir "${distdir}"
+  echo " ---- after mkdir ---- "
+  ls -la
   glide install
   CGO_ENABLED=0 go build -v -o ${distdir}/$1
+  echo " ---- finished ---- "
+  ls -la ${distdir}/$1
 }
 
 go_build
