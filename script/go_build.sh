886cedbb180c604630add9acd9b07081834a7def
Effi <effoeffi@gmail.com>
2016-12-31 17:27:51 +0200

    fix go_build script;

--- script/go_build.sh
+++ script/go_build.sh
@@ -5,11 +5,7 @@ go_build() {
   rm -rf "${distdir}"
   mkdir "${distdir}"
   glide install
-  echo " ---- after glide install ---- "
   CGO_ENABLED=0 go build -v -o ${distdir}/$1
-  echo " ---- finished ---- "
-  echo ${distdir}/$1
-  ls -la ${distdir}/$1
 }
 
-go_build
+go_build $1
